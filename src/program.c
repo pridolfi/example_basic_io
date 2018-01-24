617e12f3a930093e524375ff2a35b38975c3daf0
Pablo <pabloridolfi@gmail.com>
2018-01-24 20:37:04 -0300

    add example
    Signed-off-by: Pablo <pabloridolfi@gmail.com>

---
+++ src/program.c
@@ -0,0 +1,30 @@
+#define IO_COUNT 8
+
+const int outs[IO_COUNT] = {21,20,16,12,25,24,23,18};
+const int ins[IO_COUNT] = {17,27,22,5,6,13,19,26};
+
+void setup(void)
+{
+    for (int i=0; i<IO_COUNT; i++) {
+	    pinMode(outs[i], OUTPUT);
+	    pinMode(ins[i], INPUT);
+    }
+}
+
+void loop(void)
+{
+    for (int i=0; i<IO_COUNT; i++) {
+        digitalWrite(outs[i], !digitalRead(ins[i]));
+        delay(10);
+#if 0
+        if (!digitalRead(ins[i])) {
+            digitalWrite(outs[i], HIGH);
+        } else {
+            digitalWrite(outs[i], HIGH);
+    	    delay(500);
+    	    digitalWrite(outs[i], LOW);
+    	    delay(500);
+        }
+#endif
+    }
+}
